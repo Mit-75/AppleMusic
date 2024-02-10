a2723e9806447d837868bce35f477e8f2f71ec11
Dmitry Parkhomenko <oku_74@mail.ru>
2024-02-10 19:08:51 +0500

    Настройка экрана SongsTableViewController, настройка ячейки и названия ячейки

---
+++ AppleMusic/Views/TrackListCell.swift
@@ -0,0 +1,23 @@
+//
+//  TrackListCell.swift
+//  AppleMusic
+//
+//  Created by Dmitry Parhomenko on 10.02.2024.
+//
+
+import UIKit
+
+class TrackListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
